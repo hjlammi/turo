8ab3ca051846ba2060ec843977094cea950ba294
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2019-03-13 19:35:01 +0200

    Create Codepipeline and Codebuild

---
+++ terraform/cicd.tf
@@ -0,0 +1,216 @@
+locals {
+  pipeline_name = "turo-pipeline"
+  github_username = "hjlammi"
+  github_repo = "turo"
+}
+
+data "aws_ssm_parameter" "github_token" {
+  name = "github_token"
+}
+
+# CodePipeline resources
+resource "aws_s3_bucket" "build_artifact_bucket" {
+  bucket = "${local.pipeline_name}-artifact-bucket"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "turo_lambda" {
+  bucket = "turo-lambda"
+}
+
+data "aws_iam_policy_document" "codepipeline_assume_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name               = "${local.pipeline_name}-codepipeline-role"
+  assume_role_policy = "${data.aws_iam_policy_document.codepipeline_assume_policy.json}"
+}
+
+# CodePipeline policy needed to use CodeCommit and CodeBuild
+resource "aws_iam_role_policy" "attach_codepipeline_policy" {
+  name = "${local.pipeline_name}-codepipeline-policy"
+  role = "${aws_iam_role.codepipeline_role.id}"
+
+  policy = <<EOF
+{
+    "Statement": [
+        {
+            "Action": [
+                "s3:GetObject",
+                "s3:GetObjectVersion",
+                "s3:GetBucketVersioning",
+                "s3:PutObject"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        },
+        {
+            "Action": [
+                "cloudwatch:*",
+                "sns:*",
+                "sqs:*",
+                "iam:PassRole"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        },
+        {
+            "Action": [
+                "codebuild:BatchGetBuilds",
+                "codebuild:StartBuild"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        }
+    ],
+    "Version": "2012-10-17"
+}
+EOF
+}
+
+# CodeBuild IAM Permissions
+resource "aws_iam_role" "codebuild_assume_role" {
+  name = "${local.pipeline_name}-codebuild-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codebuild_policy" {
+  name = "${local.pipeline_name}-codebuild-policy"
+  role = "${aws_iam_role.codebuild_assume_role.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+       "s3:PutObject",
+       "s3:GetObject",
+       "s3:GetObjectVersion",
+       "s3:GetBucketVersioning",
+       "s3:ListBucket",
+       "s3:ListObjects"
+      ],
+      "Resource": "*",
+      "Effect": "Allow"
+    },
+    {
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_codebuild_project.build_project.id}"
+      ],
+      "Action": [
+        "codebuild:*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Resource": [
+        "*"
+      ],
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+    }
+  ]
+}
+POLICY
+}
+
+# CodeBuild Section for the Package stage
+resource "aws_codebuild_project" "build_project" {
+  name          = "${local.pipeline_name}-build"
+  description   = "The CodeBuild project for ${local.pipeline_name}"
+  service_role  = "${aws_iam_role.codebuild_assume_role.arn}"
+  build_timeout = "60"
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "aws/codebuild/nodejs:6.3.1"
+    type         = "LINUX_CONTAINER"
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "${file("${path.root}/buildspec.yml")}"
+  }
+}
+
+# Full CodePipeline
+resource "aws_codepipeline" "codepipeline" {
+  name     = "${local.pipeline_name}-codepipeline"
+  role_arn = "${aws_iam_role.codepipeline_role.arn}"
+
+  artifact_store = {
+    location = "${aws_s3_bucket.build_artifact_bucket.bucket}"
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["code"]
+
+      configuration {
+        Owner                = "${local.github_username}"
+        OAuthToken           = "${data.aws_ssm_parameter.github_token.value}"
+        Repo                 = "${local.github_repo}"
+        Branch               = "master"
+        PollForSourceChanges = "true"
+      }
+    }
+  }
+
+  stage {
+    name = "DeployToS3"
+
+    action {
+      name             = "DeployToS3"
+      category         = "Test"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["code"]
+      output_artifacts = ["deployed"]
+      version          = "1"
+
+      configuration {
+        ProjectName = "${aws_codebuild_project.build_project.name}"
+      }
+    }
+  }
+}
