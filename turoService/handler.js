d5ce416f29857fb6b369b242b6e972e3f8233fab
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2019-03-19 19:43:00 +0200

    Change lambda handler message for testing

--- turoService/handler.js
+++ turoService/handler.js
@@ -4,7 +4,7 @@ module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello Serverless testingtestingtestingtestingtestingtesting!',
+      message: 'Hello Serverless testingtestingtestingtestingtesting!',
       input: event,
     }),
   };
