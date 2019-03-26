a8e0d42038b025df7ef4e1fdf9942a27d02a2d62
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2019-03-26 20:43:11 +0200

    Configure VPC and subnets

--- terraform/db.tf
+++ terraform/db.tf
@@ -11,4 +11,10 @@ resource "aws_db_instance" "turo_db" {
   name                 = "turo_db"
   username             = "db_admin"
   password             = "${data.aws_ssm_parameter.db_admin_password.value}"
+  db_subnet_group_name = "${aws_db_subnet_group.main.id}"
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "main"
+  subnet_ids = ["${aws_subnet.primary.id}", "${aws_subnet.secondary.id}"]
 }
---
+++ terraform/nat.tf
@@ -0,0 +1,72 @@
+data "aws_ami" "nat" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-vpc-nat-*"]
+  }
+}
+
+resource "aws_security_group" "nat" {
+  vpc_id = "${aws_vpc.turo.id}"
+  name_prefix = "nat"
+  ingress {
+    description = "Private subnet"
+    protocol = -1
+    from_port = 0
+    to_port = 0
+    cidr_blocks = ["${aws_vpc.turo.cidr_block}"]
+  }
+  egress {
+    description = "Internet access"
+    protocol = -1
+    from_port = 0
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags {
+    Name = "NAT"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_network_interface" "nat" {
+  subnet_id = "${aws_subnet.public.id}"
+  security_groups = ["${aws_security_group.nat.id}"]
+  source_dest_check = false
+  tags {
+    Name = "NAT"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_eip" "instance" {
+  vpc = true
+  network_interface = "${aws_network_interface.nat.id}"
+  tags {
+    Name = "NAT"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_instance" "nat" {
+  instance_type = "t2.nano"
+  ami = "${data.aws_ami.nat.id}"
+  network_interface {
+    device_index = 0
+    network_interface_id = "${aws_network_interface.nat.id}"
+  }
+  tags {
+    Name = "NAT"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
