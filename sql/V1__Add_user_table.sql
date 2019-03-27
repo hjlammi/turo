01ed12e5da228d65fc8c8cd032231099fff31799
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2019-03-27 19:34:07 +0200

    Change reserved table name

--- sql/V1__Add_user_table.sql
+++ sql/V1__Add_user_table.sql
@@ -1,5 +1,5 @@
-create table USER (
-    ID int NOT NULL,
-    NAME varchar(100) NOT NULL,
-    PASSWORD varchar(100) NOT NULL
+CREATE TABLE "user" (
+    id int NOT NULL,
+    name varchar(100) NOT NULL,
+    password varchar(100) NOT NULL
 );
