9ae87dccbc6c6eb6504b902246ece86d72db0bf6
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2019-04-26 11:26:23 +0300

    Use bcrypt library to hash user password

--- backend/src/user.js
+++ backend/src/user.js
@@ -1,14 +1,19 @@
+const bcrypt = require('bcrypt');
+
 exports.create = async (db, username, password) => {
   const result = await db.query(
     'SELECT username FROM "user" WHERE username = $1',
     [username],
   );
 
+  const saltRounds = 10;
+  const hash = await bcrypt.hash(password, saltRounds);
+
   // Insert new user into the user table if a user with the same username doesn't already exist.
   if (result.rowCount === 0) {
     await db.query(
       'INSERT INTO "user"(username, password) VALUES($1, $2)',
-      [username, password],
+      [username, hash],
     );
 
     return true;
@@ -27,5 +32,10 @@ exports.authenticate = async (db, username, password) => {
     return false;
   }
 
-  return password === result.rows[0].password;
+  const match = await bcrypt.compare(password, result.rows[0].password);
+  if (match) {
+    return true;
+  }
+
+  return false;
 };
