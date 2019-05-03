84f6434e345f0f70b9190cc2b712206467d54e92
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2019-05-03 19:59:53 +0300

    Add disabled prop to CustomButton

--- frontend/turo-app/src/components/CustomButton.js
+++ frontend/turo-app/src/components/CustomButton.js
@@ -5,11 +5,17 @@ const CustomButton = (props) => {
   CustomButton.propTypes = {
     buttonText: PropTypes.string.isRequired,
     id: PropTypes.string.isRequired,
+    disabled: PropTypes.bool,
   };
 
-  const { buttonText, id } = props;
+  const { buttonText, id, disabled } = props;
+
+  CustomButton.defaultProps = {
+    disabled: false,
+  };
+
   return (
-    <button className="button" id={id} type="submit">
+    <button className="button" id={id} type="submit" disabled={disabled}>
       {buttonText}
     </button>
   );
