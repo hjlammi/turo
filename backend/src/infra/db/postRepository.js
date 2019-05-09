c50f5e49d0c932a0d08c3b90a89ec3e97a7f5d1b
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2019-05-09 15:56:41 +0300

    Fetch all posts in desc order by timestamp

--- backend/src/infra/db/postRepository.js
+++ backend/src/infra/db/postRepository.js
@@ -16,6 +16,14 @@ exports.findById = async (db, id) => {
   return result.rows[0];
 };
 
+exports.fetchAll = async (db) => {
+  const result = await db.query(
+    'SELECT * FROM "post" ORDER BY created DESC',
+  );
+
+  return result.rows;
+};
+
 exports.deleteAll = async (db) => {
   await db.query(
     'DELETE FROM post',
--- backend/test/domain/postTest.js
+++ backend/test/domain/postTest.js
@@ -7,11 +7,26 @@ const { db, withDb } = require('../dbInit.js');
 describe('Post creation', function () {
   withDb();
 
-  it('creates a new post', async function () {
+  let userId;
+  beforeEach(async function () {
     const userResult = await userRepository.add(db(), 'alice', 'alice@example.com', 'alices_password');
-    const postResult = await post.create(db(), 'test post', userResult.id);
+    userId = userResult.id;
+  });
+
+  it('creates a new post', async function () {
+    const postResult = await post.create(db(), 'test post1', userId);
     const result = await postRepository.findById(db(), postResult.id);
-    expect(result.creator).to.equal(userResult.id);
-    expect(result.content).to.equal('test post');
+    expect(result.creator).to.equal(userId);
+    expect(result.content).to.equal('test post1');
+  });
+
+  it('fetches all created posts', async function () {
+    await post.create(db(), 'test post1', userId);
+    await post.create(db(), 'test post2', userId);
+    await post.create(db(), 'test post3', userId);
+    const result = await postRepository.fetchAll(db());
+    expect(result.length).to.equal(3);
+    expect(result[0].created).to.be.gt(result[1].created);
+    expect(result[1].created).to.be.gt(result[2].created);
   });
 });
