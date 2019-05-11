e6d2bbaab1737314eef041c34c0d1b4f9cece3b2
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2019-05-11 16:01:04 +0300

    Preserve cookie in homepage test

--- frontend/turo-app/cypress/integration/home.spec.js
+++ frontend/turo-app/cypress/integration/home.spec.js
@@ -8,19 +8,27 @@ context('Home', () => {
     cy.get('#loginButton').click();
   });
 
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce('connect.sid');
+  });
+
   // After logging in, Alice doesn't see the welcome text anymore but there is
   // a logout link in the header
   describe('header component', () => {
     it('should not have the welcome text in the header', () => {
       cy.get('.header').should('not.have.text', 'Welcome to turo-app!');
     })
+  });
 
-  })
-
-  // There is an input field for writing a post and Alice decides to write her first post.
+  // There is an input field for writing a post and Alice decides to write her first posts.
   describe('writing a post', () => {
-    it('should show a new post on top of the posts feed', () => {
+    it('should show a new post written by the user on top of the posts feed', () => {
       cy.get('#post').type('This is my first post, yay!');
+      cy.get('button').click();
+      cy.get('li:first-child .content').should('have.text', 'This is my first post, yay!');
+      cy.get('#post').type('This is my second post!');
+      cy.get('button').click();
+      cy.get('li:first-child .content').should('have.text', 'This is my second post!');
     })
   });
 
