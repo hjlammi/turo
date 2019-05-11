4f3d54137061d780e5103bf9a93d0c062dd6ae47
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2019-05-11 19:15:49 +0300

    Get db connection data from .env in tests

--- backend/test/dbInit.js
+++ backend/test/dbInit.js
@@ -1,6 +1,7 @@
 const pg = require('pg');
 const postRepository = require('../src/infra/db/postRepository.js');
 const userRepository = require('../src/infra/db/userRepository.js');
+require('dotenv').config();
 
 let db;
 
@@ -9,11 +10,11 @@ exports.db = () => db;
 exports.withDb = async () => {
   before(async function () {
     db = new pg.Client({
-      host: 'localhost',
-      port: 5432,
-      database: 'turo_db',
-      user: 'turo',
-      password: 'turo123',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     });
 
     await db.connect();
