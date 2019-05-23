35a32562bfb8c72c0e32fade08f985b6af1d06bc
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2019-05-23 18:58:11 +0300

    Get db access data from param store in production

--- backend/src/presentation/handler.js
+++ backend/src/presentation/handler.js
@@ -5,152 +5,214 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const PGSession = require('connect-pg-simple')(session);
 const csrf = require('csurf');
+const AWS = require('aws-sdk');
 
 const userService = require('../application/userService');
 const postService = require('../application/postService');
 
-const dbPool = new pg.Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+let handler;
+const initHandler = async () => {
+  let parameters;
+  let host;
+  let port;
+  let database;
+  let dbUser;
+  let dbPassword;
+  let sessionSecret;
 
-const app = express();
-app.use(bodyParser.json()); // for parsing application/json
-
-const sess = {
-  store: new PGSession({ pool: dbPool }),
-  secret: process.env.SESSION_SECRET,
-  cookie: { maxAge: 24 * 60 * 60 * 1000 }, // 24 hours
-  resave: false,
-  saveUninitialized: true,
-};
-
-const withDb = async (f) => {
-  const db = await dbPool.connect();
-
-  try {
-    await f(db);
-  } finally {
-    await db.release();
+  if (handler) {
+    return;
   }
-};
 
-if (app.get('env') === 'production') {
-  sess.cookie.secure = true;
-}
+  const app = express();
+  if (app.get('env') === 'production') {
+    const params = {
+      Names: [
+        'turo_db_admin_password',
+        'turo_db_database',
+        'turo_db_host',
+        'turo_db_port',
+        'turo_db_user',
+        'turo_session_secret',
+      ],
+      withDecryption: true,
+    };
+    parameters = await AWS.ssm.getParameters(params).promise();
 
-app.use(session(sess));
+    host = parameters.turo_db_host;
+    port = parameters.turo_db_port;
+    database = parameters.turo_db_database;
+    dbUser = parameters.turo_db_user;
+    dbPassword = parameters.turo_db_admin_password;
+    sessionSecret = parameters.session_secret;
+  } else {
+    host = process.env.DB_HOST;
+    port = process.env.DB_PORT;
+    database = process.env.DB_DATABASE;
+    dbUser = process.env.DB_USER;
+    dbPassword = process.env.DB_PASSWORD;
+    sessionSecret = process.env.SESSION_SECRET;
+  }
 
-const e2eRouter = () => {
-  const e2e = new express.Router();
-
-  /* eslint-disable global-require */
-  const e2eUserService = require('../application/e2e/userService');
-  /* eslint-enable global-require */
-  e2e.delete('/users', async (req, res) => {
-    await withDb(async (db) => {
-      await e2eUserService.deleteAll(db);
-      res.sendStatus(200);
-    });
+  const dbPool = new pg.Pool({
+    host,
+    port,
+    database,
+    user: dbUser,
+    password: dbPassword,
   });
 
-  return e2e;
-};
+  app.use(bodyParser.json()); // for parsing application/json
 
-// Used only in dev env for emptying db for tests.
-// Must be disabled for production!
-if (process.env.E2E_API_ENABLED) {
-  app.use('/e2e', e2eRouter());
-}
+  const sess = {
+    store: new PGSession({ pool: dbPool }),
+    secret: sessionSecret,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 }, // 24 hours
+    resave: false,
+    saveUninitialized: true,
+  };
 
-app.use(csrf());
-app.use((err, req, res, next) => {
-  if (err.code === 'EBADCSRFTOKEN') {
-    res.sendStatus(403);
-  } else {
-    next(err);
+  const withDb = async (f) => {
+    const db = await dbPool.connect();
+
+    try {
+      await f(db);
+    } finally {
+      await db.release();
+    }
+  };
+
+  if (app.get('env') === 'production') {
+    sess.cookie.secure = true;
   }
-});
 
-app.get('/csrf-token', async (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.status(200).send(csrfToken);
-});
+  app.use(session(sess));
 
-app.post('/users/register', async (req, res) => {
-  const { username, email, password } = req.body;
+  const e2eRouter = () => {
+    const e2e = new express.Router();
 
-  const validEmail = email.match(/.@./g);
-
-  if (!validEmail) {
-    res.status(400).json({ error: 'INVALID_EMAIL' });
-  } else if (!username.match(/^\w+$/)) {
-    res.status(400).json({ error: 'INVALID_USERNAME' });
-  } else if (password.length < 10) {
-    res.status(400).json({ error: 'TOO_SHORT_PASSWORD' });
-  } else {
-    await withDb(async (db) => {
-      const result = await userService.register(db, username, email, password);
-      if (result.success) {
-        res.status(200).json({ success: result.success });
-      } else if (result.error === 'USERNAME_TAKEN') {
-        res.status(409).json({ error: result.error });
-      } else if (result.error === 'EMAIL_TAKEN') {
-        res.status(409).json({ error: result.error });
-      } else {
-        res.sendStatus(500);
-      }
+    /* eslint-disable global-require */
+    const e2eUserService = require('../application/e2e/userService');
+    /* eslint-enable global-require */
+    e2e.delete('/users', async (req, res) => {
+      await withDb(async (db) => {
+        await e2eUserService.deleteAll(db);
+        res.sendStatus(200);
+      });
     });
+
+    return e2e;
+  };
+
+  // Used only in dev env for emptying db for tests.
+  // Must be disabled for production!
+  if (process.env.E2E_API_ENABLED) {
+    app.use('/e2e', e2eRouter());
   }
-});
 
-app.post('/users/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  await withDb(async (db) => {
-    const user = await userService.login(db, email, password);
-    if (user) {
-      req.session.userId = user.id;
-      res.json(user).status(200);
+  app.use(csrf());
+  app.use((err, req, res, next) => {
+    if (err.code === 'EBADCSRFTOKEN') {
+      res.sendStatus(403);
     } else {
-      res.sendStatus(400);
+      next(err);
     }
   });
-});
 
-app.post('/users/logout', async (req, res) => {
-  req.session.destroy();
-  res.send(200);
-});
+  app.get('/csrf-token', async (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.status(200).send(csrfToken);
+  });
 
-app.get('/users/me', async (req, res) => {
-  // If session has userId stored it means that user is logged in
-  // (session hasn't been destroy by user logging out or session being expired)
-  if (req.session.userId) {
+  app.post('/users/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    const validEmail = email.match(/.@./g);
+
+    if (!validEmail) {
+      res.status(400).json({ error: 'INVALID_EMAIL' });
+    } else if (!username.match(/^\w+$/)) {
+      res.status(400).json({ error: 'INVALID_USERNAME' });
+    } else if (password.length < 10) {
+      res.status(400).json({ error: 'TOO_SHORT_PASSWORD' });
+    } else {
+      await withDb(async (db) => {
+        const result = await userService.register(db, username, email, password);
+        if (result.success) {
+          res.status(200).json({ success: result.success });
+        } else if (result.error === 'USERNAME_TAKEN') {
+          res.status(409).json({ error: result.error });
+        } else if (result.error === 'EMAIL_TAKEN') {
+          res.status(409).json({ error: result.error });
+        } else {
+          res.sendStatus(500);
+        }
+      });
+    }
+  });
+
+  app.post('/users/login', async (req, res) => {
+    const { email, password } = req.body;
+
     await withDb(async (db) => {
-      const user = await userService.getUserData(db, req.session.userId);
+      const user = await userService.login(db, email, password);
       if (user) {
+        req.session.userId = user.id;
         res.json(user).status(200);
       } else {
-        res.sendStatus(404);
+        res.sendStatus(400);
       }
     });
-  } else {
-    res.sendStatus(403);
-  }
-});
+  });
 
-app.post('/posts', async (req, res) => {
-  const { post } = req.body;
-  if (post.length <= 500) {
+  app.post('/users/logout', async (req, res) => {
+    req.session.destroy();
+    res.send(200);
+  });
+
+  app.get('/users/me', async (req, res) => {
+    // If session has userId stored it means that user is logged in
+    // (session hasn't been destroy by user logging out or session being expired)
     if (req.session.userId) {
       await withDb(async (db) => {
-        const result = await postService.create(db, post, req.session.userId);
+        const user = await userService.getUserData(db, req.session.userId);
+        if (user) {
+          res.json(user).status(200);
+        } else {
+          res.sendStatus(404);
+        }
+      });
+    } else {
+      res.sendStatus(403);
+    }
+  });
+
+  app.post('/posts', async (req, res) => {
+    const { post } = req.body;
+    if (post.length <= 500) {
+      if (req.session.userId) {
+        await withDb(async (db) => {
+          const result = await postService.create(db, post, req.session.userId);
+          if (result) {
+            res.sendStatus(200);
+          } else {
+            res.sendStatus(500);
+          }
+        });
+      } else {
+        res.sendStatus(403);
+      }
+    } else {
+      res.status(413).send('TOO_LONG_POST');
+    }
+  });
+
+  app.get('/posts', async (req, res) => {
+    if (req.session.userId) {
+      await withDb(async (db) => {
+        const result = await postService.fetchAll(db);
         if (result) {
-          res.sendStatus(200);
+          res.json(result).status(200);
         } else {
           res.sendStatus(500);
         }
@@ -158,24 +220,12 @@ app.post('/posts', async (req, res) => {
     } else {
       res.sendStatus(403);
     }
-  } else {
-    res.status(413).send('TOO_LONG_POST');
-  }
-});
+  });
 
-app.get('/posts', async (req, res) => {
-  if (req.session.userId) {
-    await withDb(async (db) => {
-      const result = await postService.fetchAll(db);
-      if (result) {
-        res.json(result).status(200);
-      } else {
-        res.sendStatus(500);
-      }
-    });
-  } else {
-    res.sendStatus(403);
-  }
-});
+  handler = serverless(app);
+};
 
-module.exports.handler = serverless(app);
+module.exports.handler = async (event, context) => {
+  await initHandler();
+  return handler(event, context);
+};
