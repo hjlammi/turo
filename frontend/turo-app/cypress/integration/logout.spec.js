9be73d62903455db44c90e7eee21544c17d8cb85
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2019-05-28 14:09:20 +0300

    Move login in its own test in Cypress logout spec

--- frontend/turo-app/cypress/integration/logout.spec.js
+++ frontend/turo-app/cypress/integration/logout.spec.js
@@ -15,23 +15,20 @@ context('Logout', () => {
             password: 'alices_password'
           }
         });
-        cy.request({
-          method: 'POST',
-          url: 'http://localhost:4000/users/login',
-          headers: {
-            'csrf-token': response.body
-          },
-          body: {
-            email: 'alice@example.com',
-            password: 'alices_password'
-          }
-        });
+
         cy.visit('http://localhost:3000/#/');
     });
   });
 
   // Alice is logged in and wants to log out from the application.
   describe('logout functionality', () => {
+    it ('should log in', () => {
+      cy.get('#email').type('alice@example.com');
+      cy.get('#loginButton').should('be.disabled');
+      cy.get('#password').type('alices_password');
+      cy.get('#loginButton').click();
+      cy.location('hash').should('eq', '#/');
+    });
     // She clicks the logout link and is logged out from the application and redirected to the login page.
     it('should redirect to the login page when logout link clicked', () => {
       cy.get('.link').click();
